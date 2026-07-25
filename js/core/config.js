d16884cd33f6db539f1d42130b8f374bbe03234b
Deepak Chauhan <144633357+de2pakchauhan@users.noreply.github.com>
2026-07-25 11:37:09 +0530

    Update API_URL and GOOGLE_CLIENT_ID in config.js

--- js/core/config.js
+++ js/core/config.js
@@ -17,13 +17,13 @@ export const Config = Object.freeze({
     // Google Apps Script Web App URL
     // ----------------------------
 
-    API_URL: "",          // <-- Paste your Apps Script /exec URL here
+    API_URL: "https://script.google.com/macros/s/AKfycbx8RQ4IP9sCldFxyOJSwMIF-5ekZ9LqaSB1sZPcqbDJkPO8BAUgR1m2uw9AaH9b8o7vOA/exec",          // <-- Paste your Apps Script /exec URL here
 
     // ----------------------------
     // Google OAuth2 Client ID
     // ----------------------------
 
-    GOOGLE_CLIENT_ID: "", // <-- Paste your OAuth 2.0 Client ID here
+    GOOGLE_CLIENT_ID: "401103632011-qgjvt6fko9knb651oe6b89rrs12fgobk.apps.googleusercontent.com", // <-- Paste your OAuth 2.0 Client ID here
 
     // ----------------------------
     // Network
